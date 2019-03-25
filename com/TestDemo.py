4d2355bd64867b99bcb20262ce646d7d02d22426
vikas <vikas.kamble@afourtech.com>
2019-03-25 10:53:41 +0530

    Hi

--- com/TestDemo.py
+++ com/TestDemo.py
@@ -1,2 +1,20 @@
-print("Hello World...")
-print("This is a my local branch test")
+
+import lemoncheesecake.api as lcc
+
+SUITE = {
+    "description": "My Awesome Test Suite",
+    "rank": 1
+}
+
+
+@lcc.test("My First Test")
+def verifyMyFirstTest():
+    lcc.log.info("Inside my first Test")
+
+@lcc.test("My Second Test")
+def verifyMySecondTest():
+    lcc.log.info("Inside my first Test")
+
+def teardown_suite():
+        lcc.log_info("Do this after the suite...")
+        print("Test")
---
+++ com/TestPython.py
@@ -0,0 +1,16 @@
+import math
+
+# Cube of number
+
+x = int(input("Enter thr number : "))
+print("Cube of number is : ", math.pow(x,3))
+
+# Even Odd Number
+
+a= 141
+if a%2==0:
+    print("Even Nuber.")
+    if (a%2==0)>5:
+        print("Greater than 5.")
+else:
+    print("Odd Number.")
